5ec806aa8bd9550978d6194142f6a16192c4c6aa
Kristopher Andrews <kristopherca@gmail.com>
2019-01-18 17:47:32 -0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'readwrite.dart';
+import 'dart:async';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ReadWrite(),
+    );
+  }
+}
+
+class ReadWrite extends StatefulWidget {
+  @override
+  _ReadWriteState createState() => _ReadWriteState();
+}
+
+class _ReadWriteState extends State<ReadWrite> {
+  var _dataEntry = TextEditingController();
+  var message = " ";
+
+  void assign() async {
+    setState(() {});
+
+    message = await readData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Read/Write'),
+        centerTitle: true,
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(13.4),
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: TextField(
+                controller: _dataEntry,
+                decoration: InputDecoration(labelText: 'Enter data'),
+              ),
+            ),
+            ListTile(
+              title: RaisedButton(
+                  color: Colors.green,
+                  onPressed: () {
+                    //SAVE TO FILE
+                    assign();
+                    writeData(_dataEntry.text);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          'Save Data',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 50.0),
+                        ),
+                        Padding(padding: EdgeInsets.all(14.5)),
+                        Text('$message')
+                      ],
+                    ),
+                  )),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/readwrite.dart
@@ -0,0 +1,29 @@
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'dart:async';
+
+Future<String> get _localPath async {
+  final directory = await getApplicationDocumentsDirectory();
+  return directory.path; //home/directory/
+}
+
+Future<File> get _localFile async {
+  final path = await _localPath;
+  return File('$path/data.txt'); //home/directory/data.txt
+}
+
+Future<File> writeData(String message) async {
+  final file = await _localFile;
+  return file.writeAsString('$message'); //write to file
+}
+
+Future<String> readData() async {
+  try {
+    final file = await _localFile;
+
+    String data = await file.readAsString(); //read file
+    return data;
+  } catch (e) {
+    return "nothing saved yet";
+  }
+}
